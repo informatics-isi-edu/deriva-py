b3344250f5782565aefdb587215ad15071d7b9a2
mikedarcy <mikedarcy@users.noreply.github.com>
2017-10-16 15:30:42 -0700

    Add BaseCLI to package-level imports.

--- deriva_py/__init__.py
+++ deriva_py/__init__.py
@@ -2,6 +2,7 @@ __version__ = "0.3.0"
 
 from deriva_py.common import *
 from deriva_py.common import datapath
+from deriva_py.common.base_cli import BaseCLI
 from deriva_py.common.deriva_binding import DerivaBinding
 from deriva_py.common.ermrest_catalog import ErmrestCatalog
 from deriva_py.common.ermrest_config import AttrDict, CatalogConfig
