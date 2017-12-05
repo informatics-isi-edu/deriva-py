fa56710cd0b437cf3b3ba82c356969c11cea5c21
mikedarcy <mikedarcy@users.noreply.github.com>
2017-12-04 21:19:19 -0800

    Deal with portalocker version pecadillos.

--- deriva/core/__init__.py
+++ deriva/core/__init__.py
@@ -9,7 +9,7 @@ import logging
 import requests
 from collections import OrderedDict
 from distutils.util import strtobool
-from portalocker import Lock, LOCK_EX, LOCK_SH
+from pkg_resources import parse_version, get_distribution, DistributionNotFound
 
 __version__ = "0.3.1"
 
@@ -146,6 +146,24 @@ def read_config(config_file=DEFAULT_CONFIG_FILE, create_default=False, default=D
     return json.loads(config, object_pairs_hook=OrderedDict)
 
 
+PORTALOCKER = None
+try:
+    PORTALOCKER = get_distribution("portalocker")
+    from portalocker import Lock, LOCK_EX, LOCK_SH
+except DistributionNotFound:
+    pass
+
+
+def lock_file(file, mode, exclusive=True):
+    if PORTALOCKER:
+        if parse_version(PORTALOCKER.version) > parse_version("0.6.1"):
+            return Lock(file, mode, timeout=60, flags=LOCK_EX if exclusive else LOCK_SH)
+        else:
+            return Lock(file, mode, timeout=60, flags=LOCK_EX if exclusive else LOCK_SH, truncate=None)
+    else:
+        return io.open(file, mode)
+
+
 def write_credential(credential_file=DEFAULT_CREDENTIAL_FILE, credential=DEFAULT_CREDENTIAL):
     credential_dir = os.path.dirname(credential_file)
     if not os.path.isdir(credential_dir):
@@ -154,7 +172,7 @@ def write_credential(credential_file=DEFAULT_CREDENTIAL_FILE, credential=DEFAULT
         except OSError as error:
             if error.errno != errno.EEXIST:
                 raise
-    with Lock(credential_file, 'w', timeout=60, flags=LOCK_EX) as cf:
+    with lock_file(credential_file, mode='w', exclusive=True) as cf:
         os.chmod(credential_file, 0o600)
         cf.write(json.dumps(credential, indent=2))
         cf.flush()
@@ -175,7 +193,7 @@ def read_credential(credential_file=DEFAULT_CREDENTIAL_FILE, create_default=Fals
             credential = default
 
     if not credential:
-        with Lock(credential_file, 'r', timeout=60, flags=LOCK_EX, truncate = None) as cf:
+        with lock_file(credential_file, mode='r', exclusive=True) as cf:
             credential = cf.read()
 
     return json.loads(credential, object_pairs_hook=OrderedDict)
@@ -195,6 +213,7 @@ def resource_path(relative_path, default=os.path.abspath(".")):
     if default is None:
         return relative_path
     return os.path.join(default, relative_path)
+
 
 from deriva.core import datapath
 from deriva.core.base_cli import BaseCLI
--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setup(
     install_requires=[
         'requests',
         'pika',
-        'portalocker'
+        'portalocker>=1.1.0'
     ],
     license='Apache 2.0',
     classifiers=[
