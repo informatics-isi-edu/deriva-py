04ca5ed7c2fd5385d9e337c0222d83afdcbeb73d
Rob Schuler <robes@users.noreply.github.com>
2018-04-10 14:42:00 -0700

    adds support for creating a catalog #25

--- deriva/core/__init__.py
+++ deriva/core/__init__.py
@@ -261,6 +261,7 @@ def get_transfer_summary(total_bytes, elapsed_time):
 
 from deriva.core.base_cli import BaseCLI, KeyValuePairArgs
 from deriva.core.deriva_binding import DerivaBinding, DerivaPathError
+from deriva.core.deriva_server import DerivaServer
 from deriva.core.ermrest_catalog import ErmrestCatalog
 from deriva.core.ermrest_config import AttrDict, CatalogConfig
 from deriva.core.polling_ermrest_catalog import PollingErmrestCatalog
