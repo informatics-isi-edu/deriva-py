b836e88a3f68f48861943c1ab7efe7657134f33e
Rob Schuler <robes@users.noreply.github.com>
2022-11-08 19:52:12 -0800

    Move configure functions to new catalog_config module

--- deriva/core/ermrest_model.py
+++ deriva/core/ermrest_model.py
@@ -261,62 +261,6 @@ class Model (object):
     @object_annotation(tag.display)
     def display(self): pass
 
-    def configure_baseline_ermrest_client(self):
-        """Baseline configuration of `ERMrest_Client` table.
-
-        Set up `ERMrest_Client` table so that it has readable names and uses the display name of the user as the row
-        name. To apply changes, `Model.apply()` must be invoked after using this method.
-        """
-        ermrest_client = self.schemas['public'].tables['ERMrest_Client']
-
-        # Make ermrest_client table visible.  If the GUID or member name is considered sensitive, then this needs to be
-        # changed.
-        ermrest_client.acls['select'] = ['*']
-
-        # Set table and row name.
-        ermrest_client.annotations.update({
-            tag.display: {'name': 'Users'},
-            tag.visible_columns: {'compact': ['Full_Name', 'Display_Name', 'Email', 'ID']},
-            tag.table_display: {'row_name': {'row_markdown_pattern': '{{{Full_Name}}}'}}
-        })
-
-        column_annotations = {
-            'RCT': {tag.display: {'name': 'Creation Time'}},
-            'RMT': {tag.display: {'name': 'Modified Time'}},
-            'RCB': {tag.display: {'name': 'Created By'}},
-            'RMB': {tag.display: {'name': 'Modified By'}}
-        }
-        for k, v in column_annotations.items():
-            ermrest_client.columns[k].annotations.update(v)
-
-    def configure_baseline_ermrest_group(self):
-        """Baseline configuration of `ERMrest_Group` table.
-        
-        Set up `ERMrest_Group` table so that it has readable names and uses the display name of the group as the row
-        name. To apply changes, `Model.apply()` must be invoked after using this method.
-        """
-        ermrest_group = self.schemas['public'].tables['ERMrest_Group']
-
-        # Make ERMrest_Group table visible. If the GUID or group name is considered sensitive, then this needs to be
-        # changed.
-        ermrest_group.acls['select'] = ['*']
-
-        # Set table and row name.
-        ermrest_group.annotations.update({
-            tag.display: {'name': 'User Groups'},
-            tag.visible_columns: {'compact': ['Display_Name', 'ID']},
-            tag.table_display: {'row_name': {'row_markdown_pattern': '{{{Display_Name}}}'}}
-        })
-
-        column_annotations = {
-            'RCT': {tag.display: {'name': 'Creation Time'}},
-            'RMT': {tag.display: {'name': 'Modified Time'}},
-            'RCB': {tag.display: {'name': 'Created By'}},
-            'RMB': {tag.display: {'name': 'Modified By'}}
-        }
-        for k, v in column_annotations.items():
-            ermrest_group.columns[k].annotations.update(v)
-
 def strip_nochange(d):
     return {
         k: v
