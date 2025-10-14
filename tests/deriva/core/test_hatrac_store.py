a56453ef8770b822983807bdc99fa7ad2eb08be9
Rob Schuler <robes@users.noreply.github.com>
2025-10-14 16:15:56 -0700

    test_hatrac_store.py added
    Initial unit tests for the hatrac store module.

--- tests/deriva/core/test_hatrac_store.py
+++ tests/deriva/core/test_hatrac_store.py
@@ -5,18 +5,18 @@
 #  DERIVA_PY_TEST_CREDENTIAL: user credential, if none, it will attempt to get credential for given hostname (optional)
 #  DERIVA_PY_TEST_VERBOSE: set for verbose logging output to stdout (optional)
 
+import io
 import logging
 import os
+import tempfile
 import unittest
-from deriva.core import DerivaServer, get_credential
+import uuid
+from deriva.core import get_credential, HatracStore
+from deriva.core.utils import hash_utils as hu
 
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.StreamHandler())
-if os.getenv("DERIVA_PY_TEST_VERBOSE"):
-    logger.setLevel(logging.DEBUG)
-else:
-    logger.setLevel(logging.INFO)
-
+logger.setLevel(logging.DEBUG if os.getenv("DERIVA_PY_TEST_VERBOSE") else logging.INFO)
 hostname = os.getenv("DERIVA_PY_TEST_HOSTNAME")
 credential = os.getenv("DERIVA_PY_TEST_CREDENTIAL") or get_credential(hostname)
 
@@ -24,38 +24,65 @@ credential = os.getenv("DERIVA_PY_TEST_CREDENTIAL") or get_credential(hostname)
 @unittest.skipUnless(hostname, "Test host not specified")
 class HatracStoreTestCase(unittest.TestCase):
 
+    base_path = '/hatrac/' + str(uuid.uuid4())
+
     @classmethod
     def setUpClass(cls):
-        server = DerivaServer('https', hostname, credentials=credential)
-        catalog_id = os.getenv("DERIVA_PY_TEST_CATALOG")
-        if catalog_id is not None:
-            logger.info(f"Reusing catalog {catalog_id} on host {hostname}")
-            cls.catalog = server.connect_ermrest(catalog_id)
-            cls._purgeCatalog()
-        else:
-            cls.catalog = server.create_ermrest_catalog()
-            logger.info(f"Created catalog {cls.catalog.catalog_id} on host {hostname}")
+        hatrac = HatracStore('https', hostname, credentials=credential)
+        hatrac.create_namespace(cls.base_path)
 
     @classmethod
     def tearDownClass(cls):
-        if cls.catalog and os.getenv("DERIVA_PY_TEST_CATALOG") is None:
-            logger.info(f"Deleting catalog {cls.catalog.catalog_id} on host {hostname}")
-            cls.catalog.delete_ermrest_catalog(really=True)
+        hatrac = HatracStore('https', hostname, credentials=credential)
+        hatrac.delete_namespace(cls.base_path)
 
     def setUp(self):
-        self.model = self.catalog.getCatalogModel()
+        self.hatrac = HatracStore('https', hostname, credentials=credential)
 
     def tearDown(self):
-        self._purgeCatalog()
+        self.hatrac = None
 
+    def test_namespace_operations(self):
+        test_path = self.base_path + '/ns_test'
+        self.assertIsNone(self.hatrac.create_namespace(test_path))
+        self.assertIn(test_path, self.hatrac.retrieve_namespace(self.base_path))
+        self.assertListEqual(self.hatrac.retrieve_namespace(test_path), [])
+        self.assertTrue(self.hatrac.is_valid_namespace(test_path))
+        self.assertIsNone(self.hatrac.delete_namespace(test_path))
+        self.assertNotIn(test_path, self.hatrac.retrieve_namespace(self.base_path))
 
+    def test_object_operations(self):
+        test_path = self.base_path + '/obj_test'
+        content = b'temporary file contents ' * 100
+        self.assertTrue(self.hatrac.put_obj(test_path, io.BytesIO(content)).startswith(test_path))
+        self.assertEqual(self.hatrac.get_obj(test_path).content, content)
+        self.assertTrue(
+            self.hatrac.content_equals(test_path, md5=hu.compute_hashes(io.BytesIO(content), hashes=['md5'])['md5'][1])
+        )
+        self.assertIsNone(self.hatrac.del_obj(test_path))
 
-import unittest
+    def test_acl_operations(self):
+        access = 'create'
+        role = 'test-role'
+        self.assertIsNone(self.hatrac.set_acl(self.base_path, access, [role], add_role=True))
+        self.assertIn(role, self.hatrac.get_acl(self.base_path, access=access)[access])
+        self.assertIsNone(self.hatrac.del_acl(self.base_path, access, role=role))
+        self.assertNotIn(role, self.hatrac.get_acl(self.base_path, access=access)[access])
 
-
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def test_chunked_upload(self):
+        test_path = self.base_path + '/chunk_test'
+        chunk_size = 1024
+        with tempfile.NamedTemporaryFile() as temp_file:
+            temp_file.write(b'temp ' * chunk_size)
+            temp_file.seek(0)
+            self.assertTrue(
+                self.hatrac.put_loc(test_path, temp_file.name, chunked=True, chunk_size=chunk_size).startswith(test_path)
+            )
+            temp_file.seek(0)
+            self.assertTrue(
+                self.hatrac.content_equals(test_path, md5=hu.compute_hashes(temp_file, hashes=['md5'])['md5'][1])
+            )
+        self.assertIsNone(self.hatrac.del_obj(test_path))
 
 
 if __name__ == '__main__':
